a605c0705a5f1833e45630d8499a67b2bb337d81
Erik <erikdmitchell@gmail.com>
2018-09-12 14:55:49 -0400

    added calc for sub menus to be full width

--- class-bmm-nav-walker.php
+++ class-bmm-nav-walker.php
@@ -35,7 +35,7 @@ class BMM_Nav_Walker extends Walker_Nav_Menu {
         // Default class to add to the file.
         $classes = array( 'bmm-sub-menu' );
         
-        $classes[] = "bmm-sub-menu-level-$depth";
+        //$classes[] = "bmm-sub-menu-level-$depth";
 
         /**
          * Filters the CSS class(es) applied to a menu list element.
