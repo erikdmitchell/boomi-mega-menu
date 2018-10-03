6547fed08ecb229724999235a160cd70975a7c0d
Erik <erikdmitchell@gmail.com>
2018-10-03 12:18:24 -0400

    Pulling in stylesheet from css folder

--- class-bmm.php
+++ class-bmm.php
@@ -97,7 +97,7 @@ final class BMM {
      * @return void
      */
     public function frontend_scripts_styles() {
-        wp_enqueue_style( 'bmm-style', BMM_URL . 'sass/bmm.css', '', $this->version );
+        wp_enqueue_style( 'bmm-style', BMM_URL . 'css/bmm.css', '', $this->version );
     }
 
     /**
