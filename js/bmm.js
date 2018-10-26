cb58180e8d6c8a6f7dadac6f7005fb4fef0787a3
Erik <erikdmitchell@gmail.com>
2018-10-26 11:33:53 -0400

    removed positionSubmenus()

--- js/bmm.js
+++ js/bmm.js
@@ -9,14 +9,12 @@ jQuery(function ($) {
     
     // run our functions.
     jQuery(document).ready(function() {
-        sizeTabpanes();
-        //positionSubmenus();        
+        sizeTabpanes();       
     });
     
     // on window resize, run functions.
     $(window).on('resize', function() {
         sizeTabpanes(); 
-        //positionSubmenus();
     });
     
     // sets the width and position of the tabpanes.
@@ -30,29 +28,6 @@ jQuery(function ($) {
                 'left': left,
                 'width': menuWidth
             });
-        });        
-    }
-    
-    // positions the submenus.
-    function positionSubmenus() {
-        $('.pmm-mega-menu-primary-nav-item.pmm-mega-menu-item-has-children').each(function () {
-            var maxMenuLinkWidth = 0;
-            var $menu = $('.pmm-mega-menu.nav-menu');
-            var $submenu = $(this).children('.pmm-mega-sub-menu');
-            var columns = $submenu.children('.pmm-mega-menu-item').data('pmmcolumns');
-    
-            // add offset of not 100% width.
-            if ($menu.width() != $submenu.outerWidth()) {
-                var left = $(this).position().left;
-                var totalWidth = $submenu.outerWidth() + Math.ceil($(this).position().left);
-    
-                // if the submenu would be pushed to wide, we move it back
-                if (totalWidth > $menu.width()) {
-                    left = left - (totalWidth - $menu.width());
-                }
-    
-                $submenu.css('left', left);
-            }
         });        
     }
     
