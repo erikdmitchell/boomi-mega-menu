e557d57533a3438713860dafc9a5e888c2decef9
Erik <erikdmitchell@gmail.com>
2019-03-07 12:23:44 -0500

    added support for sub menu positioning

--- js/bmm.js
+++ js/bmm.js
@@ -2,12 +2,14 @@ jQuery(function ($) {
 
     // run our functions.
     jQuery(document).ready(function() {
-        sizeTabpanes();       
+        sizeTabpanes();  
+        positionSubmenus();     
     });
     
     // on window resize, run functions.
     $(window).on('resize', function() {
         sizeTabpanes(); 
+        positionSubmenus();
     });
     
     // sets the width and position of the tabpanes.
@@ -27,5 +29,28 @@ jQuery(function ($) {
             });
         });        
     }
+    
+    // positions the submenus.
+    function positionSubmenus() {
+        $('.bmm-primary-nav-item.menu-item-has-children').each(function () {
+            var maxMenuLinkWidth = 0;
+            var $menu = $('.bmm-menu.nav-menu');
+            var $submenu = $(this).children('.bmm-sub-menu');
+            //var columns = $submenu.children('.pmm-mega-menu-item').data('pmmcolumns');
+    
+            // add offset of not 100% width.
+            if ($menu.width() != $submenu.outerWidth()) {
+                var left = $(this).position().left;
+                var totalWidth = $submenu.outerWidth() + Math.ceil($(this).position().left);
+    
+                // if the submenu would be pushed to wide, we move it back
+                if (totalWidth > $menu.width()) {
+                    left = left - (totalWidth - $menu.width());
+                }
+    
+                $submenu.css('left', left);
+            }
+        });        
+    }    
 
 });
