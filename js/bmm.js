b95223c4a44aca336e6a3a3782df8e56a342d725
Erik <erikdmitchell@gmail.com>
2019-05-29 13:24:23 -0400

    js meets standards

--- js/bmm.js
+++ js/bmm.js
@@ -18,7 +18,7 @@ jQuery(function ($) {
             var $menu = $(this).parents('.bmm-sub-menu.bmm-top-level-sub-menu');
             var menuWidth = $menu.outerWidth();
             var menuHeight = $menu.outerHeight();
-            var menuPadding = parseInt($menu.css('padding'));
+            var menuPadding = parseInt($menu.css('padding'), 10);
             var top = menuHeight - menuPadding;
             var left = ($(this).offset().left - $menu.offset().left) * -1;
 
@@ -33,13 +33,13 @@ jQuery(function ($) {
     // positions the submenus.
     function positionSubmenus() {
         $('.bmm-primary-nav-item.menu-item-has-children').each(function () {
-            var maxMenuLinkWidth = 0;
+            //var maxMenuLinkWidth = 0;
             var $menu = $('.bmm-menu.nav-menu');
             var $submenu = $(this).children('.bmm-sub-menu');
             //var columns = $submenu.children('.pmm-mega-menu-item').data('pmmcolumns');
     
             // add offset of not 100% width.
-            if ($menu.width() != $submenu.outerWidth()) {
+            if ($menu.width() !== $submenu.outerWidth()) {
                 var left = $(this).position().left;
                 var totalWidth = $submenu.outerWidth() + Math.ceil($(this).position().left);
     
