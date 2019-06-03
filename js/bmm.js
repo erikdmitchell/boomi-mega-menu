6dc10439b6a8931cd4b41c28f65d4fefbd68fb81
Erik <erikdmitchell@gmail.com>
2019-06-03 11:47:49 -0400

    cleaned up code

--- js/bmm.js
+++ js/bmm.js
@@ -1,20 +1,20 @@
-jQuery(function ($) {
+jQuery(function($) {
 
     // run our functions.
     jQuery(document).ready(function() {
-        sizeTabpanes();  
-        positionSubmenus();     
-    });
-    
-    // on window resize, run functions.
-    $(window).on('resize', function() {
-        sizeTabpanes(); 
+        sizeTabpanes();
         positionSubmenus();
     });
-    
+
+    // on window resize, run functions.
+    $(window).on('resize', function() {
+        sizeTabpanes();
+        positionSubmenus();
+    });
+
     // sets the width and position of the tabpanes.
     function sizeTabpanes() {
-        $('.bmm-sub-menu.bmm-tabpane').each(function () {
+        $('.bmm-sub-menu.bmm-tabpane').each(function() {
             var $menu = $(this).parents('.bmm-sub-menu.bmm-top-level-sub-menu');
             var menuWidth = $menu.outerWidth();
             var menuHeight = $menu.outerHeight();
@@ -24,33 +24,33 @@ jQuery(function ($) {
 
             $(this).css({
                 'left': left,
-                'top' : top,
+                'top': top,
                 'width': menuWidth
             });
-        });        
+        });
     }
-    
+
     // positions the submenus.
     function positionSubmenus() {
-        $('.bmm-primary-nav-item.menu-item-has-children').each(function () {
+        $('.bmm-primary-nav-item.menu-item-has-children').each(function() {
             //var maxMenuLinkWidth = 0;
             var $menu = $('.bmm-menu.nav-menu');
             var $submenu = $(this).children('.bmm-sub-menu');
             //var columns = $submenu.children('.pmm-mega-menu-item').data('pmmcolumns');
-    
+
             // add offset of not 100% width.
             if ($menu.width() !== $submenu.outerWidth()) {
                 var left = $(this).position().left;
                 var totalWidth = $submenu.outerWidth() + Math.ceil($(this).position().left);
-    
+
                 // if the submenu would be pushed to wide, we move it back
                 if (totalWidth > $menu.width()) {
                     left = left - (totalWidth - $menu.width());
                 }
-    
+
                 $submenu.css('left', left);
             }
-        });        
-    }    
+        });
+    }
 
 });
