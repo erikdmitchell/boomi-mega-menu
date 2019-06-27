4ac75e96e34b1e7643124aa253778dc2bcfdd93e
Erik <erikdmitchell@gmail.com>
2019-06-27 11:50:43 -0400

    updated version

--- class-bmm.php
+++ class-bmm.php
@@ -19,7 +19,7 @@ final class BMM {
      * @var string
      * @access public
      */
-    public $version = '0.3.3';
+    public $version = '0.4.0';
 
     /**
      * Construct function.
@@ -97,7 +97,7 @@ final class BMM {
      * @return void
      */
     public function frontend_scripts_styles() {
-        wp_enqueue_script( 'bmm-scrupt', BMM_URL . 'js/bmm.min.js', array( 'jquery' ), $this->version, false );
+        wp_enqueue_script( 'bmm-scrupt', BMM_URL . 'js/bmm.js', array( 'jquery' ), $this->version, false );
 
         wp_enqueue_style( 'bmm-style', BMM_URL . 'css/bmm.min.css', '', $this->version );
     }
